d03aa5eb3d977f54268f873fe756388c57c9efb2
Kirin Patel <kirinpatel@gmail.com>
2019-05-04 00:50:25 -0400

    Initial commit

---
+++ meh.com/meh.com/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  meh.com
+//
+//  Created by Kirin Patel on 5/4/19.
+//  Copyright © 2019 Kirin Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
