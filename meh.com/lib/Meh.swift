6195911e19940af64c7033c4cf2ef217282e2cee
Kirin Patel <kirinpatel@gmail.com>
2019-05-09 13:08:36 -0400

    Created function to get story as html

--- meh.com/lib/Meh.swift
+++ meh.com/lib/Meh.swift
@@ -6,6 +6,48 @@
 //  Copyright © 2019 Kirin Patel. All rights reserved.
 //
 
+fileprivate func defaultStyles(_ backgroundColor: String = "#FFFFFF", _ accentColor: String = "#000000") -> String {
+    return """
+    <style> \
+        body { \
+            background-color: \(backgroundColor); \
+            color: \(accentColor); \
+            font-family: Helvetica; \
+        } \
+        h1 { \
+            font-size: 50px; \
+        } \
+        p { \
+            font-size: 40px; \
+        }\
+        a, \
+        a:link, \
+        a:visited, \
+        a:link:active, \
+        a:visited:active { \
+            color: \(accentColor); \
+        } /
+    </style>
+    """
+}
+
+fileprivate func markdownLinkReplacerScript(_ elementName: String = "body") -> String {
+    return """
+    <script> \
+        let pattern = /(\\[.*?\\])(\\(.*?\\))/g; \
+        let element = document.getElementById("\(elementName)"); \
+        element.innerHTML.match(pattern).forEach(match => { \
+        let textPattern = /\\[.*?\\]/; \
+        let text = match.match(textPattern).join("").replace(/\\[|\\]/g, ""); \
+        let hrefPattern = /\\(.*?\\)/; \
+        let href = match.match(hrefPattern).join("").replace(/\\(|\\)/g, ""); \
+        let link = `<a href="${href}">${text}</a>`; \
+        element.innerHTML = element.innerHTML.replace(match, link); \
+        }); \
+    </script>
+    """
+}
+
 class Anwser: Decodable {
     var id: String?
     var text: String?
@@ -27,6 +69,30 @@ class PurchaseQuantity: Decodable {
 class Story: Decodable {
     var body: String?
     var title: String?
+    func asHTML(_ backgroundColor: String = "#FFFFFF", _ accentColor: String = "#000000") -> String? {
+        if let body = body, let title = title {
+            var parsedBody = body.replacingOccurrences(of: "\r", with: "")
+            parsedBody = parsedBody.replacingOccurrences(of: "\n", with: "<br>")
+            parsedBody = parsedBody.replacingOccurrences(of: "\'", with: "'")
+            return """
+            <!doctype html> \
+            <html lang="en"> \
+                <head> \
+                    <meta charset="utf-8"> \
+                    <title>eh for meh story</title> \
+                </head> \
+                \(defaultStyles(backgroundColor, accentColor)) \
+                <body> \
+                    <h1>\(title)</h1> \
+                    <p id="body">\(parsedBody)</p> \
+                    \(markdownLinkReplacerScript()) \
+                </body> \
+            </html>
+            """
+        } else {
+            return nil
+        }
+    }
 }
 
 @objc class Theme: NSObject, Decodable {
